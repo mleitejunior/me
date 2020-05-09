8c120028dec6b8b54e7824f7dc78530736bf29ca
Marcelo Leite Junior <mleitejunior@gmail.com>
2020-05-09 07:23:14 -0300

    test

---
+++ js/google-map/map-active.js
@@ -0,0 +1,40 @@
+var map;
+var latlng = new google.maps.LatLng(-22.7698491, -47.1559885);
+var stylez = [{
+    featureType: "all",
+    elementType: "all",
+    stylers: [{
+        saturation: -10
+            }]
+        }];
+var mapOptions = {
+    zoom: 15,
+    center: latlng,
+    scrollwheel: false,
+    scaleControl: false,
+    disableDefaultUI: true,
+    mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'gMap']
+    }
+};
+map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+var geocoder_map = new google.maps.Geocoder();
+var address = 'Machado';
+geocoder_map.geocode({
+    'address': address
+}, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: map.getCenter()
+        });
+    } else {
+        alert("Geocode was not successful for the following reason: " + status);
+    }
+});
+var mapType = new google.maps.StyledMapType(stylez, {
+    name: "Grayscale"
+});
+map.mapTypes.set('gMap', mapType);
+map.setMapTypeId('gMap');
